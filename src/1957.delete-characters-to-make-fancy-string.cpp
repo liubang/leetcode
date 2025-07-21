6a68a8979ae2a901e7e9b239aeb4a9c0b6b073a3
liubang <it.liubang@gmail.com>
2025-07-21 12:27:56 +0800

    update

--- src/1957.delete-characters-to-make-fancy-string.cpp
+++ src/1957.delete-characters-to-make-fancy-string.cpp
@@ -44,7 +44,8 @@ public:
 // @leet end
 
 } // namespace
-TEST(Leetcode, aaa) {
+
+TEST(Leetcode, delete_characters_to_make_fancy_string) {
     Solution s;
     EXPECT_EQ("leetcode", s.makeFancyString("leeetcode"));
     EXPECT_EQ("aabaa", s.makeFancyString("aaabaaaa"));
